5ed21ae55f8bf5cfb7894ac1d450cef9d4e76d17
Julie Gunawan <julie.gunawan@protonmail.com>
2024-03-20 09:54:30 -0600

    fix the alignment of sidebar for user and admin

--- carbon-auto/src/app/ui/user-dashboard/sidenav.tsx
+++ carbon-auto/src/app/ui/user-dashboard/sidenav.tsx
@@ -1,29 +1,38 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/admin-dashboard/nav-links';
-// import { PowerIcon } from '@heroicons/react/24/outline';
 import DashboardLogo from '../dashboard-logo';
+import { Box, Button, Drawer, Toolbar, Typography } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
 
 export default function SideNav() {
+  const drawerWidth = 200;
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
-      >
-        <div className="w-32 text-white md:w-40">
-          <DashboardLogo />
-        </div>
+    <Drawer 
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Link href="/">
+        <DashboardLogo />
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          
-            <div className="hidden md:block">Sign Out</div>
-          </button>
+          <Button variant='contained'> Sign Out </Button>
         </form>
-      </div>
-    </div>
+      </Box>
+    </Drawer>
   );
 }
--- carbon-auto/src/app/userDashboard/page.tsx
+++ carbon-auto/src/app/userDashboard/page.tsx
@@ -3,7 +3,6 @@ import { UserProfile } from "@/app/ui/user-dashboard/user-profile";
 export default function UserDashboardPage() {
     return (
     <>
-      <p>user dashboard Page</p>
       <UserProfile />
     </>
     )
