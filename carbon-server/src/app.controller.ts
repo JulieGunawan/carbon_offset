81b9bf8960724d45e2f25edff7737273624a7b0b
Julie Gunawan <julie.gunawan@protonmail.com>
2024-03-25 11:21:31 -0600

    update controller and services

--- carbon-server/src/app.controller.ts
+++ carbon-server/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('')
+  getCat(): any {
+    // return this.appService.getHello();
+    const cat = new Cat("kitty", 8);
+    return cat;
   }
+
+  @Post('')
+  createCat(@Body() body): any {
+    const cat = new Cat(body.name, body.age);
+    cats.push(cat);
+    return cat;
+  }
+}
+
+let cats = [];
+
+class Cat {
+  constructor(private name: string, private age: number) {}
 }
--- carbon-server/src/app.service.ts
+++ carbon-server/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+//service uses injectable to inject it to the controller
 @Injectable()
 export class AppService {
   getHello(): string {
